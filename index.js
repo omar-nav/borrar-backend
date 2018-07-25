d2d9d72dca5d09a33a4a8e90e4219129f2c4df7e
omar-nav <omar.navarro@g.austincc.edu>
2018-07-24 20:00:18 -0500

    first commit

--- index.js
+++ index.js
@@ -1,46 +1,53 @@
-let axios = require ('axios');
+let axios = require('axios');
 let app = require('express')();
 
 let port = process.env.PORT || 5000;
 
 app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
-
 
 app.get('/', (req, res)=>{
-   res.send('Hola desde pokeapi');
+    res.send('Hola desde pokeapi');
 });
 
-app.get('/pokemons', (req, res) => {
+app.get('/pokemons', (req, res)=>{
+    // axios.get().the().catch;
 
-   let pokeIds = [1, 2, 3];
+    let pokeIds = [1,2,3];
 
-   let promesasIncompletas = pokeIds.map((id)=>{
+    console.log('entre a peticion');
+    
 
-       return axios.get(`http://pokeapi.co/api/v2/pokemon/${id}/`)
-       .then((response)=>{
-           let data = response.data;
-           let name = data.name;
-           let img = data.sprites.front_default;
-           let pokemon = { name, img };
-           return pokemon;
-   
-       }).catch((error)=>res.send(error));
+    let promesasIncompletas = pokeIds.map((id)=>{
 
-   });
+        return axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        .then((response)=>{
+            let data = response.data;
+            let name = data.name;
+            let img = data.sprites.front_default;
+            let pokemon = { name, img }
+            console.log('entre al map');
+            
+            return pokemon;
+        })
+        .catch((error)=>{
+            res.send(error);
+        });
+    });
 
-   Promise.all(promesasIncompletas).then((pokemones)=>{
+    //Las promesas incompletas son el "map" de promesas. Dentro de cada promesa viene un objeto de pokemon (nombre e imagen). El Promise.all se ejecutará hasta que se resuelvan todos los elementos.
+    Promise.all(promesasIncompletas).then((pokemones)=>{
+        console.log('acabe todo');
+        
+        res.send(pokemones);
+    });
 
-       res.send(pokemones);
-   });
-   
 });
 
 app.listen(port, ()=>{
-   console.log(`todo bien, 
-   eres el mejor`, `Me levante en 
-   el puerto ${port}`);
-});
+    console.log(`Todo bien, eres el mejor =*`, `Me levanté en el puerto ${port}`);
+    
+})
